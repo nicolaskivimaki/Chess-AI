44845ba1c2d40e27ba141514a38b32420ec4938d
Nicolas <kivimakinicolas@gmail.com>
2023-01-29 20:17:17 +0200

    board and pieces

---
+++ src/board.py
@@ -0,0 +1,104 @@
+
+
+from sqlalchemy import false
+
+
+class Board():
+
+    def __init__(self):
+    
+        self.board_state = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+
+        self.white_to_move = True
+        self.moveLog = []
+    
+    def make_move(self, start_square, end_square):
+        print(start_square, end_square)
+        start_row = start_square[1]
+        start_col = start_square[0]
+        end_row = end_square[1]
+        end_col = end_square[0]
+
+        to_move = self.board_state[start_row][start_col]
+
+        if to_move == "--":
+            return
+
+        self.board_state[end_row][end_col] = to_move
+        self.board_state[start_row][start_col] = "--"
+    
+    def change_turn(self):
+        if self.white_to_move:
+            self.white_to_move = False
+        else:
+            self.white_to_move = True
+    
+    def check_selected(self, x, y):
+        square = self.board_state[y][x]
+
+        if square == "--":
+            return True
+        else:
+            return False
+
+    def check_move(self, move):
+        print("Requested move:", move)
+        possible_moves = self.get_all_moves()
+        if move in possible_moves:
+            return True
+        return False
+
+    def get_all_moves(self):
+        moves = []
+        for i in range(8):
+            for j in range(8):
+                color = self.board_state[j][i][0]
+                if (color == "w" and self.white_to_move) or (color =="b" and not self.white_to_move):
+                    piece = self.board_state[j][i][1]
+                    if piece == "P":
+                        print("White pawn")
+                        moves.append(self.get_pawn_moves(i, j, moves))
+        return moves
+
+    def get_pawn_moves(self, cols, rows, moves):
+
+        print("OOR", cols-1, rows-1)
+        print(self.board_state[cols][rows])
+
+        if self.white_to_move:
+            if self.board_state[rows-1][cols] == "--":
+                moves.append([(cols, rows), (cols, rows-1)])
+                if self.board_state[rows-2][cols] == "--" and rows == 6:
+                    moves.append([(cols, rows), (cols, rows-2)])
+            if cols in range (0, 8):
+                if self.board_state[rows-1][cols-1][0] == "b":
+                    moves.append([(cols, rows), (cols-1, rows-1)])
+            if cols in range (0, 7):
+                if self.board_state[rows-1][cols+1][0] == "b":
+                    moves.append([(cols, rows), (cols+1, rows-1)])
+
+        else:
+            if rows in range(0, 7):
+                if self.board_state[rows+1][cols] == "--":
+                    moves.append([(cols, rows), (cols, rows+1)])
+                    if self.board_state[rows+2][cols] == "--" and rows == 1:
+                        moves.append([(cols, rows), (cols, rows+2)])
+                if cols in range (1, 7):
+                    if self.board_state[rows+1][cols-1][0] == "w":
+                        moves.append([(cols, rows), (cols-1, rows+1)])
+                if cols in range (0, 7):
+                    if self.board_state[rows+1][cols+1][0] == "w":
+                        moves.append([(cols, rows), (cols+1, rows+1)])
+
+        print("possible moves:", moves)
+        return moves
+
---
+++ src/game.py
@@ -0,0 +1,80 @@
+import pygame
+from board import Board
+from constants import *
+import sys
+import os
+dirname = os.path.dirname(__file__)
+
+class Game():
+
+    def __init__(self):
+        self.clock = pygame.time.Clock()
+        self.board = Board()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    def game_loop(self):
+
+        self.load_images()
+        self.screen.fill(pygame.Color("white"))
+        print(self.board.board_state)
+        selected_squares = ()
+        clicks = []
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    location = pygame.mouse.get_pos()
+                    x = location[0] // SQUARE_SIZE
+                    y = location[1] // SQUARE_SIZE
+                    if selected_squares == (x, y):
+                        selected_squares = ()
+                        clicks = []
+                    if len(clicks) == 0 and self.board.check_selected(x, y):
+                        selected_squares = ()
+                        clicks = []
+                    else:
+                        selected_squares = (x, y)
+                        clicks.append(selected_squares)
+                    if len(clicks) == 2:
+                        if self.board.check_move(clicks):
+                            self.board.make_move(clicks[0], clicks[1])
+                            self.board.change_turn()
+                            clicks = []
+                        else:
+                            selected_squares = ()
+                            clicks = []
+
+            self.draw_game()
+            self.clock.tick(FPS)
+            pygame.display.flip()
+    
+    def load_images(self):
+        pieces = ["bR", "bN", "bB", "bQ", "bK", "bP", "wR", "wN", "wB", "wQ", "wK", "wP"]
+
+        for piece in pieces:
+            IMAGES[piece] = pygame.transform.scale(pygame.image.load(os.path.join(dirname, "assets", f"{piece}.png")), (SQUARE_SIZE, SQUARE_SIZE))
+            
+    
+    def draw_game(self):
+
+        self.draw_board()
+        self.drawpieces()
+    
+    def draw_board(self):
+
+        for i in range(DIMENSIONS):
+            for j in range(DIMENSIONS):
+                if (i + j) % 2 == 0:
+                    pygame.draw.rect(self.screen, WHITE, (i*SQUARE_SIZE, j*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                else:
+                    pygame.draw.rect(self.screen, GREEN, (i*SQUARE_SIZE, j*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+    def drawpieces(self):
+        for i in range(DIMENSIONS):
+            for j in range(DIMENSIONS):
+                piece = self.board.board_state[j][i]
+                if piece != "--":
+                    self.screen.blit(IMAGES[piece], pygame.Rect(i*SQUARE_SIZE, j*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
