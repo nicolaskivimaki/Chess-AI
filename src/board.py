d300238c33c15527c86853309180729656fd08f4
Nicolas <kivimakinicolas@gmail.com>
2023-02-08 14:03:05 +0200

    movement for knights, rooks, queen, king

--- src/board.py
+++ src/board.py
@@ -97,6 +97,15 @@ class Board():
                         moves.append(self.get_pawn_moves(i, j, moves))
                     if piece == "B":
                         moves.append(self.get_bishop_moves(i, j, moves))
+                    if piece == "R":
+                        moves.append(self.get_rook_moves(i, j, moves))
+                    if piece == "N":
+                        moves.append(self.get_knight_moves(i, j, moves))
+                    if piece == "K":
+                        moves.append(self.get_king_moves(i, j, moves))
+                    if piece == "Q":
+                        moves.append(self.get_queen_moves(i, j, moves))
+
         return moves
 
     def get_pawn_moves(self, cols, rows, moves):
@@ -110,29 +119,28 @@ class Board():
         if self.white_to_move:
             if self.board_state[rows-1][cols] == "--":
                 moves.append([(cols, rows), (cols, rows-1)])
-                if self.board_state[rows-2][cols] == "--" and rows == 6:
+                if rows >= 6 and self.board_state[rows-2][cols] == "--":
                     moves.append([(cols, rows), (cols, rows-2)])
-            if cols in range (0, 8):
+            if cols in range(0, 8):
                 if self.board_state[rows-1][cols-1][0] == "b":
                     moves.append([(cols, rows), (cols-1, rows-1)])
-            if cols in range (0, 7):
+            if cols in range(0, 7):
                 if self.board_state[rows-1][cols+1][0] == "b":
                     moves.append([(cols, rows), (cols+1, rows-1)])
-
         else:
-            if rows in range(0, 7):
+            if rows + 1 < len(self.board_state):
                 if self.board_state[rows+1][cols] == "--":
                     moves.append([(cols, rows), (cols, rows+1)])
-                    if self.board_state[rows+2][cols] == "--" and rows == 1:
+                    if rows <= 1 and self.board_state[rows+2][cols] == "--" and rows+2 < len(self.board_state):
                         moves.append([(cols, rows), (cols, rows+2)])
-                if cols in range (1, 7):
+                if cols in range(0, 7):
                     if self.board_state[rows+1][cols-1][0] == "w":
                         moves.append([(cols, rows), (cols-1, rows+1)])
-                if cols in range (0, 7):
+                if cols in range(0, 7):
                     if self.board_state[rows+1][cols+1][0] == "w":
                         moves.append([(cols, rows), (cols+1, rows+1)])
-
         return moves
+
 
     def get_bishop_moves(self, cols, rows, moves):
         """
@@ -146,7 +154,7 @@ class Board():
                 moves.append([(cols, rows), (col, row)])
                 row -= 1
                 col -= 1
-            elif self.board_state[row][col][0] == "w" if self.white_to_move else "b":
+            elif self.board_state[row][col][0] == "b" if self.white_to_move else "w":
                 moves.append([(cols, rows), (col, row)])
                 break
             else:
@@ -159,7 +167,7 @@ class Board():
                 moves.append([(cols, rows), (col, row)])
                 row -= 1
                 col += 1
-            elif self.board_state[row][col][0] == "w" if self.white_to_move else "b":
+            elif self.board_state[row][col][0] == "b" if self.white_to_move else "w":
                 moves.append([(cols, rows), (col, row)])
                 break
             else:
@@ -172,7 +180,7 @@ class Board():
                 moves.append([(cols, rows), (col, row)])
                 row += 1
                 col -= 1
-            elif self.board_state[row][col][0] == "w" if self.white_to_move else "b":
+            elif self.board_state[row][col][0] == "b" if self.white_to_move else "w":
                 moves.append([(cols, rows), (col, row)])
                 break
             else:
@@ -185,10 +193,95 @@ class Board():
                 moves.append([(cols, rows), (col, row)])
                 row += 1
                 col += 1
-            elif self.board_state[row][col][0] == "w" if self.white_to_move else "b":
+            elif self.board_state[row][col][0] == "b" if self.white_to_move else "w":
                 moves.append([(cols, rows), (col, row)])
                 break
             else:
                 break
 
         return moves
+
+    def get_rook_moves(self, cols, rows, moves):
+        """
+        Takes the location of a rook and returns all possible
+        moves considering the chess rules.
+        """
+        color = "w" if self.white_to_move else "b"
+        enemy_color = "b" if self.white_to_move else "w"
+
+        # Check up direction
+        for row in range(rows-1, -1, -1):
+            if self.board_state[row][cols] == "--":
+                moves.append([(cols, rows), (cols, row)])
+            elif self.board_state[row][cols][0] == enemy_color:
+                moves.append([(cols, rows), (cols, row)])
+                break
+            else:
+                break
+            
+        # Check down direction
+        for row in range(rows+1, 8):
+            if self.board_state[row][cols] == "--":
+                moves.append([(cols, rows), (cols, row)])
+            elif self.board_state[row][cols][0] == enemy_color:
+                moves.append([(cols, rows), (cols, row)])
+                break
+            else:
+                break
+
+        # Check left direction
+        for col in range(cols-1, -1, -1):
+            if self.board_state[rows][col] == "--":
+                moves.append([(cols, rows), (col, rows)])
+            elif self.board_state[rows][col][0] == enemy_color:
+                moves.append([(cols, rows), (col, rows)])
+                break
+            else:
+                break
+
+        # Check right direction
+        for col in range(cols+1, 8):
+            if self.board_state[rows][col] == "--":
+                moves.append([(cols, rows), (col, rows)])
+            elif self.board_state[rows][col][0] == enemy_color:
+                moves.append([(cols, rows), (col, rows)])
+                break
+            else:
+                break
+
+        return moves
+
+    def get_knight_moves(self, cols, rows, moves):
+
+        """
+        Takes the location of a knight and returns all possible
+        moves considering it's surrounding squares and
+        chess rules.
+        """
+        movements = [
+            [-2, -1], [-2, 1], [-1, -2], [-1, 2],
+            [1, -2], [1, 2], [2, -1], [2, 1]
+        ]
+
+        for movement in movements:
+            if (0 <= rows + movement[0] < 8) and (0 <= cols + movement[1] < 8):
+                end_square = (cols + movement[1], rows + movement[0])
+                if self.board_state[end_square[1]][end_square[0]][0] != self.board_state[rows][cols][0]:
+                    moves.append([(cols, rows), end_square])
+        return moves
+
+    def get_king_moves(self, col, row, moves):
+
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                new_col = col + i
+                new_row = row + j
+                if new_col in range(0, 8) and new_row in range(0, 8):
+                    destination = self.board_state[new_row][new_col]
+                    if destination == "--" or destination[0] != self.board_state[row][col][0]:
+                        moves.append([(col, row), (new_col, new_row)])
+        return moves
+
+    def get_queen_moves(self, col, row, moves):
+        moves.append(self.get_bishop_moves(col, row, moves))
+        moves.append(self.get_rook_moves(col, row, moves))
